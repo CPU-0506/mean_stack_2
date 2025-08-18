bf5f7832b5c5b05e47a14d1b92c865b6aa2adfbc
mohanbhimaneni <bhmohan01@gmail.com>
2025-08-18 19:58:33 +0530

    2C

--- expt2/angular-conditionals/src/app/app.ts
+++ expt2/angular-conditionals/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,FormsModule],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
