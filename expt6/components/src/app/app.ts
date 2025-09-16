9a575bfb6c9f036154483e7428d9fef7dfee0f35
mohanbhimaneni <bhmohan01@gmail.com>
2025-09-16 17:30:20 +0530

    6A

---
+++ expt6/components/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CourseList } from './course-list/course-list';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CourseList],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('components');
+  sub_name='';
+  submit(name:string){
+    this.sub_name=name;
+  }
+}
