8a8fb7f21866da54d0a5c5eaf73b109b710209f8
mohanbhimaneni <bhmohan01@gmail.com>
2025-09-16 19:18:52 +0530

    6D

--- expt6/components/src/app/app.ts
+++ expt6/components/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseList } from './course-list/course-list';
 
@@ -8,7 +8,7 @@ import { CourseList } from './course-list/course-list';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   protected readonly title = signal('components');
   sub_name='';
   registered_courses:string[]=[];
@@ -17,5 +17,40 @@ export class App {
   }
   receive_registration($event:string){
     this.registered_courses.push($event);
+  }
+  constructor() {
+    console.log('App: constructor');
+  }
+
+  ngOnInit() {
+    console.log('App: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('App: ngOnChanges', changes);
+  }
+
+  ngDoCheck() {
+    console.log('App: ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('App: ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('App: ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('App: ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('App: ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('App: ngOnDestroy');
   }
 }
