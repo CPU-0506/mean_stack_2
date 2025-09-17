a0ade3e30254f78f02b90cd80dc6be73b375f5d7
mohanbhimaneni <bhmohan01@gmail.com>
2025-09-17 18:52:31 +0530

    7A

---
+++ expt7/forms/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CourseRegistrationForm } from './course-registration-form/course-registration-form';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CourseRegistrationForm],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('forms');
+}
