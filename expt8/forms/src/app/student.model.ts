1ffa1ba42f1cae640ccc4ee70a57fba1df2f0626
mohanbhimaneni <bhmohan01@gmail.com>
2025-09-19 17:59:07 +0530

    8A

---
+++ expt8/forms/src/app/student.model.ts
@@ -0,0 +1,8 @@
+export class Student {
+    constructor(
+        public id:string,
+        public name:string,
+        public email:string,
+        public registered_courses:string[]
+    ){}
+}
